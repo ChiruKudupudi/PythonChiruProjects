aa7a64045c8e095cf5befd2a7a7c9a438f22cb07
ChiruKudupudi <kchiranjeevi60@gmail.com>
2024-06-25 16:42:33 +0530

    streamlitapptest.py

--- streamlitapptest.py
+++ streamlitapptest.py
@@ -1,1 +1,10 @@
 #code
+
+def load_data(url):
+    df = pd.read_csv(url)
+    return df
+
+df = load_data("https://github.com/plotly/datasets/raw/master/uber-rides-data1.csv")
+st.dataframe(df)
+
+st.button("Rerun")
