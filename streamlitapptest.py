df8207b19119e73bf81a83e69c9afd6f4edf3a78
ChiruKudupudi <kchiranjeevi60@gmail.com>
2024-06-25 16:55:32 +0530

    streamlitapptest.py

--- streamlitapptest.py
+++ streamlitapptest.py
@@ -1,4 +1,5 @@
 #code
+"""
 import streamlit as st
 import pandas as pd
 @st.cache_data
@@ -10,3 +11,19 @@ df = load_data("https://github.com/plotly/datasets/raw/master/uber-rides-data1.c
 st.dataframe(df)
 
 st.button("Rerun")
+"""
+# Set web page format
+import streamlit as st
+st.set_page_config(layout="wide")
+# Diasble virtual VRAM on windows system
+import torch
+torch.cuda.set_per_process_memory_fraction(0.999, 0)
+
+
+st.markdown("""
+# DiffSynth Studio
+
+[Source Code](https://github.com/Artiprocher/DiffSynth-Studio)
+
+Welcome to DiffSynth Studio.
+""")
