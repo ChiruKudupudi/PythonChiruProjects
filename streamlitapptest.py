915d29022dc1e9e33723220145793d049d239567
ChiruKudupudi <kchiranjeevi60@gmail.com>
2024-06-25 17:05:05 +0530

    streamlitapptest.py

--- streamlitapptest.py
+++ streamlitapptest.py
@@ -11,18 +11,67 @@
 
 #st.button("Rerun")
 
-# Set web page format
 import streamlit as st
-st.set_page_config(layout="wide")
-# Diasble virtual VRAM on windows system
-import torch
-torch.cuda.set_per_process_memory_fraction(0.999, 0)
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+st.title("Mortgage Repayments Calculator")
+
+st.write("### Input Data")
+col1, col2 = st.columns(2)
+home_value = col1.number_input("Home Value", min_value=0, value=500000)
+deposit = col1.number_input("Deposit", min_value=0, value=100000)
+interest_rate = col2.number_input("Interest Rate (in %)", min_value=0.0, value=5.5)
+loan_term = col2.number_input("Loan Term (in years)", min_value=1, value=30)
+
+# Calculate the repayments.
+loan_amount = home_value - deposit
+monthly_interest_rate = (interest_rate / 100) / 12
+number_of_payments = loan_term * 12
+monthly_payment = (
+    loan_amount
+    * (monthly_interest_rate * (1 + monthly_interest_rate) ** number_of_payments)
+    / ((1 + monthly_interest_rate) ** number_of_payments - 1)
+)
+
+# Display the repayments.
+total_payments = monthly_payment * number_of_payments
+total_interest = total_payments - loan_amount
+
+st.write("### Repayments")
+col1, col2, col3 = st.columns(3)
+col1.metric(label="Monthly Repayments", value=f"${monthly_payment:,.2f}")
+col2.metric(label="Total Repayments", value=f"${total_payments:,.0f}")
+col3.metric(label="Total Interest", value=f"${total_interest:,.0f}")
 
 
-st.markdown("""
-# DiffSynth Studio
+# Create a data-frame with the payment schedule.
+schedule = []
+remaining_balance = loan_amount
 
-[Source Code](https://github.com/Artiprocher/DiffSynth-Studio)
+for i in range(1, number_of_payments + 1):
+    interest_payment = remaining_balance * monthly_interest_rate
+    principal_payment = monthly_payment - interest_payment
+    remaining_balance -= principal_payment
+    year = math.ceil(i / 12)  # Calculate the year into the loan
+    schedule.append(
+        [
+            i,
+            monthly_payment,
+            principal_payment,
+            interest_payment,
+            remaining_balance,
+            year,
+        ]
+    )
 
-Welcome to DiffSynth Studio.
-""")
+df = pd.DataFrame(
+    schedule,
+    columns=["Month", "Payment", "Principal", "Interest", "Remaining Balance", "Year"],
+)
+
+# Display the data-frame as a chart.
+st.write("### Payment Schedule")
+payments_df = df[["Year", "Remaining Balance"]].groupby("Year").min()
+st.line_chart(payments_df)
